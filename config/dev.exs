85a45e023f001dc3cd0f6bce1dbbbe1695cc485e
Naiker <yinho999@gmail.com>
2025-11-11 01:50:52 +0000

    Update project configuration to integrate Ash framework and related dependencies
    - Updated `.formatter.exs` to include Ash-related dependencies and changed the formatter plugin.
    - Modified `mix.exs` to add `ash_postgres`, `ash`, and `sourceror` as dependencies, and updated the setup alias.
    - Enhanced `config/config.exs` with Ash and Spark configurations for policies and formatting.
    - Updated `config/dev.exs` and `config/test.exs` to include Ash policy settings.
    - Refactored `Tunez.Repo` to use `AshPostgres.Repo` and implemented required callbacks.
    - Added migration for initializing Ash extensions and created a snapshot for installed extensions.

--- config/dev.exs
+++ config/dev.exs
@@ -1,9 +1,10 @@
 import Config
+config :ash, policies: [show_policy_breakdowns?: true]
 
 # Configure your database
 config :tunez, Tunez.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "loco",
+  password: "password",
   hostname: "localhost",
   database: "tunez_dev",
   stacktrace: true,
