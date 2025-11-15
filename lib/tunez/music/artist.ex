fe801ff847fa2a834ea9bae0c8e6d6b70835a46c
Naiker <yinho999@gmail.com>
2025-11-15 02:39:20 +0000

    Add GIN index on artist name and enable pg_trgm extension for improved search functionality

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -4,6 +4,10 @@ defmodule Tunez.Music.Artist do
   postgres do
     table "artists"
     repo Tunez.Repo
+
+    custom_indexes do
+      index "name gin_trgm_ops", name: "artists_name_gin_index", using: "GIN"
+    end
   end
 
   actions do
@@ -22,6 +26,15 @@ defmodule Tunez.Music.Artist do
     # destroy :destroy do
     # end
     defaults [:read, :destroy, create: :*]
+
+    read :search do
+      argument :query, :ci_string do
+        constraints allow_empty?: true
+        default ""
+      end
+
+      filter expr(contains(name, ^arg(:query)))
+    end
 
     update :update do
       require_atomic? false
